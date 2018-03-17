54b4b0793693b9227be7a2f6a7877e56097e96d6
Gregory Norton <freezedown@hotmail.com>
2018-03-17 00:18:26 -0400

    Correced syntax errors and added functionality

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,51 @@
 extern crate interfaces;
 
 use interfaces::Interface;
+use std::process::Command;
 
 #[cfg(target_os = "windows")]
 const BROWSER: &str = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
 
-#[cfg(target_os = "unix")]
+#[cfg(target_os = "linux")]
 const BROWSER: &str = "firefox";
 
 
 fn main() {
+    //print_ifaces();
     iface_up();
+    launch_browser();
+    iface_down();
+}
+
+fn iface_up() {
+    println!("Bring interfaces up");
+    //let interfaces = Interface::get_all();
+    match Interface::get_all() {
+        Ok(mut interfaces) => {
+            for ref mut iface in &mut interfaces {
+                println!("{:?}", iface.set_up(true));
+                println!("{:?}\n\n", iface.is_up());
+            }
+        },
+        //Err(err) => println!("No interfaces detected"),
+        Err(err) => println!("{}", err),
+    }
+}
+
+fn iface_down() {
+    println!("Bring interfaces down");
+    match Interface::get_all() {
+        Ok(mut interfaces) => {
+            for ref mut iface in &mut interfaces {
+                println!("{:?}", iface.set_up(false));
+                println!("{:?}\n\n", iface.is_up());
+            }
+        },
+        Err(err) => println!("{}", err),
+    }
+}
+
+fn launch_browser() {
     match Command::new(BROWSER).spawn() {
         Ok(mut child) => {
             println!("{:?}", child);
@@ -18,49 +53,15 @@ fn main() {
         },
         Err(err) => println!("{}", err),
     }
-    //iface_down();
 }
 
-fn iface_up() {
-    println!("Bring interfaces up");
-    //let interfaces = Interface::get_all();
+fn print_ifaces() {
     match Interface::get_all() {
-        Some(interfaces) => {
+        Ok(interfaces) => {
             for ref iface in &interfaces {
-                println!("{:?}", iface);
+                println!("{}\tis up: {}", iface.name, iface.is_up());
             }
-        },
-        None => println!("No interfaces detected");
+        }
+        Err(err) => println!("{}", err),
     }
 }
-
-/*fn iface_up() {
-    println!("Bringing interfaces up");
-    let interfaces = datalink::interfaces();
-    for ref iface in &interfaces {
-        if !iface.is_up() {
-            #[cfg(target_os = "windows")]
-            {
-                let command: &str = &format!("netsh interface set interface \"{}\" ENABLE", iface.name);
-                Command::new("cmd").args(&["/C", command]).output();
-            }
-        }
-        //println!("{} is up? {}", iface.name, iface.is_up());
-    }
-}*/
-
-/*fn iface_down() {
-    println!("Bringing interfaces down");
-    let interfaces = datalink::interfaces();
-    for ref iface in &interfaces {
-        if iface.is_up() {
-            #[cfg(target_os = "windows")]
-            {
-                let command: &str = &format!("netsh interface set interface \"{}\" DISABLE", iface.name);
-                Command::new("cmd").args(&["/C", command]).output();
-            }
-            
-        }
-        //println!("{} is up? {}", iface.name, iface.is_up());
-    }
-}*/
